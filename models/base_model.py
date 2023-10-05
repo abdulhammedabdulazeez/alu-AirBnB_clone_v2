8551cfa61795da19ec854d6950e5c2e4ceceb807
unknown <a.abdulazee@alustudent.com>
2023-10-05 13:20:38 +0200

    Debugging

--- models/base_model.py
+++ models/base_model.py
@@ -57,12 +57,7 @@ class BaseModel:
         """
         return self.__str__()
 
-    def save(self):
-        """updates the public instance attribute updated_at to current
-        """
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+
 
     def to_dict(self):
         """creates dictionary of the class  and returns
