4327ae8d5e3b9447128ef57227abb25de63316a0
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-10-06 11:52:21 +0200

    debugging;

--- models/user.py
+++ models/user.py
@@ -17,10 +17,10 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column(str(128), nullable=False)
+    password = Column(str(128), nullable=False)
+    first_name = Column(str(128))
+    last_name = Column(str(128))
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
